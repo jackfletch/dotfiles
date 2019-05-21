0129e2302a06b9ab65eb56776547590c6e536538
Jack Fletcher <jdfletch97@gmail.com>
2019-05-20 23:09:45 -0500

    Add missing directory

--- .tmux/gg.sh
+++ .tmux/gg.sh
@@ -6,10 +6,10 @@ tmux send-keys 'cd $HOME/workspace/senior-design/golf-glove/' Enter
 # tmux new-window -d -a -t gg:bash -n servers
 tmux new-window -d -n servers
 tmux select-window -t gg:servers
-tmux send-keys 'cd $HOME/workspace/senior-design/golf-glove/backend/' Enter
+tmux send-keys 'cd $HOME/workspace/senior-design/golf-glove/coachApp/backend/' Enter
 # tmux split-window -t gg:servers -v
 tmux split-window -v
-tmux send-keys 'cd $HOME/workspace/senior-design/golf-glove/frontend/' Enter
+tmux send-keys 'cd $HOME/workspace/senior-design/golf-glove/coachApp/frontend/' Enter
 # tmux split-window -h 'go-golff'
 # tmux new-window -d 'go-golf'
 
